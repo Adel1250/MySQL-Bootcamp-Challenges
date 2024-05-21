ead686e7c0b4f8bc89650aaf52adcc2e659ed873
Adel1250 <adelashrafhamouda@gmail.com>
2024-05-21 15:25:30 +0300

    Format queries

--- Shirts-DB.sql
+++ Shirts-DB.sql
@@ -1,12 +1,14 @@
-create database shirts_db;
-create table shirts(
-	shirt_id int auto_increment primary key,
-    article varchar(50) not null,
-    color varchar(50) not null,
-    shirt_size varchar(2) not null,
-    last_worn int not null
+CREATE DATABASE shirts_db;
+
+CREATE TABLE shirts (
+    shirt_id INT AUTO_INCREMENT PRIMARY KEY,
+    article VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    shirt_size VARCHAR(2) NOT NULL,
+    last_worn INT NOT NULL
 );
-insert into shirts(article, color, shirt_size, last_worn) values ('t-shirt', 'white', 'S', 10),
+
+INSERT INTO shirts(article, color, shirt_size, last_worn) VALUES ('t-shirt', 'white', 'S', 10),
 ('t-shirt', 'green', 'S', 200),
 ('polo shirt', 'black', 'M', 10),
 ('tank top', 'blue', 'S', 50),
@@ -14,15 +16,53 @@ insert into shirts(article, color, shirt_size, last_worn) values ('t-shirt', 'wh
 ('polo shirt', 'red', 'M', 5),
 ('tank top', 'white', 'S', 200),
 ('tank top', 'blue', 'M', 15);
-select * from shirts;
-insert into shirts(color, article, shirt_size, last_worn) values('purple', 'polo shirt', 'M', 50);
-select article, color from shirts;
-select article, color, shirt_size, last_worn from shirts;
-update shirts set shirt_size = 'L' where article = 'polo shirt';
-update shirts set last_worn = 0 where last_worn = 15;
-update shirts set shirt_size = 'XS', color = 'off white' where color = 'white';
-delete from shirts where last_worn = 200;
-delete from shirts where article = 'tank top';
-delete from shirts;
-drop table shirts;
-drop database shirts_db;
+
+SELECT 
+    *
+FROM
+    shirts;
+    
+INSERT INTO shirts(color, article, shirt_size, last_worn) VALUES('purple', 'polo shirt', 'M', 50);
+
+SELECT 
+    article, color
+FROM
+    shirts;
+    
+SELECT 
+    article, color, shirt_size, last_worn
+FROM
+    shirts;
+    
+UPDATE shirts 
+SET 
+    shirt_size = 'L'
+WHERE
+    article = 'polo shirt';
+    
+UPDATE shirts 
+SET 
+    last_worn = 0
+WHERE
+    last_worn = 15;
+    
+UPDATE shirts 
+SET 
+    shirt_size = 'XS',
+    color = 'off white'
+WHERE
+    color = 'white';
+    
+DELETE FROM shirts 
+WHERE
+    last_worn = 200;
+    
+DELETE FROM shirts 
+WHERE
+    article = 'tank top';
+    
+DELETE FROM shirts;
+
+DROP TABLE shirts;
+
+DROP DATABASE shirts_db;
